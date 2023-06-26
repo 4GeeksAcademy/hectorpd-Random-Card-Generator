57e82e6aae3df9dc8a259d560fcbd5d80047a170
Hector Padron <helpad93@gmail.com>
2023-06-26 20:06:20 +0000

    iniciado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  cardsGenerator(palo, numero);
+}; //write your code here
+let cardsGenerator = (suit, number) => {
+  let suitIndex = Math.floor(Math.random() * suit.length);
+  let numberIndex = Math.floor(Math.random() * number.length);
+
+  let cardsuit = suit[suitIndex];
+  let cardnumber = number[numberIndex];
+
+  let cardHeader = document.getElementById("cardheader");
+  cardHeader.innerHTML = cardsuit;
+  let cardBody = document.getElementById("cardbody");
+  cardBody.innerHTML = cardnumber;
+  let cardFooter = document.getElementById("cardfooter");
+  cardFooter.innerHTML = cardsuit;
+  //cardHeader.classList.add("red");
 };
+
+let palo = ["♣", "♦", "♥", "♠"];
+let numero = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
