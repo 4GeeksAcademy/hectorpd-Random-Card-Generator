ba33f88511907b80adf29ddb7fe0d44682443e20
Hector Padron <helpad93@gmail.com>
2023-06-28 19:56:06 +0000

    finalizado

--- src/app.js
+++ src/app.js
@@ -6,8 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  // Obtener referencias a los elementos de entrada de texto
+  let widthInput = document.getElementById("widthInput");
+  let heightInput = document.getElementById("heightInput");
+
+  // Escuchar el evento "input" en los elementos de entrada de texto
+  widthInput.addEventListener("input", updateCardSize);
+  heightInput.addEventListener("input", updateCardSize);
   cardsGenerator(palo, numero);
-}; //write your code here
+};
+//write your code here
+//Definicion de la funcion a ejecutar
 let cardsGenerator = (suit, number) => {
   let suitIndex = Math.floor(Math.random() * suit.length);
   let numberIndex = Math.floor(Math.random() * number.length);
@@ -22,12 +31,36 @@ let cardsGenerator = (suit, number) => {
   let cardFooter = document.getElementById("cardfooter");
   cardFooter.innerHTML = cardsuit;
 
-  //headerCard.classList.add("red");
+  //Cambio del color segun el suit
+  if (cardsuit === "♦" || cardsuit === "♥") {
+    cardHeader.classList.add("text-danger");
+    cardFooter.classList.add("text-danger");
+  } else {
+    cardHeader.classList.remove("text-danger");
+    cardFooter.classList.remove("text-danger");
+  }
 
+  // Temporizador
+  setTimeout(() => {
+    document.querySelector("#btn").disabled = false;
+    cardsGenerator(suit, number);
+  }, 5000);
+
+  //llamado del boton
   document.querySelector("#btn").addEventListener("click", () => {
     cardsGenerator(palo, numero);
   });
 };
+function updateCardSize() {
+  // Obtener los valores de ancho y alto ingresados por el usuario
+  let width = parseInt(document.getElementById("widthInput").value);
+  let height = parseInt(document.getElementById("heightInput").value);
+
+  // Actualizar el tamaño de la carta
+  let card = document.getElementById("card");
+  card.style.width = width + "px";
+  card.style.height = height + "px";
+}
 
 let palo = ["♣", "♦", "♥", "♠"];
 let numero = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
